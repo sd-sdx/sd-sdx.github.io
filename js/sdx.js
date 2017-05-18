d9843e3f37c3b70abe9955021becb89e40e21548
Scott Tran <scott.tran@gmail.com>
2017-05-17 20:29:51 -0700

    v1

--- js/sdx.js
+++ js/sdx.js
@@ -2,12 +2,31 @@ require([
     'js/jquery.js',
     'js/handlebars.js',
     'text!session.handlebars',
-    'text!sessions.json'
-], function (_, handlebars,  sessionTemplate, data) {
+    'text!sessions.json',
+    'text!companies.handlebars',
+    'text!companies.json'
+], function (_, handlebars,  sessionTemplate, sessionData, companyTemplate, companyData) {
     var template = handlebars.compile(sessionTemplate);
+    var companyHandlebar= handlebars.compile(companyTemplate);
 
-    $("#manager-sessions")
-        .append(template( {session: JSON.parse(data)}));
+    var content = JSON.parse(sessionData);
+    var company = JSON.parse(companyData);
 
-    console.log("helllo");
+    $("#customer-support-skills")
+        .append(template( {session: content["Customers Support Skills"]}));
+    $("#manager-skills")
+        .append(template( {session: content["Manager Skills"]}));
+    $("#technical-skills")
+        .append(template( {session: content["Technical Skills"]}));
+    $("#self-skills")
+        .append(template( {session: content["Customers Support Skills"]}));
+    var ceil = Math.ceil;
+
+    Object.defineProperty(Array.prototype, 'chunk', {value: function(n) {
+        return Array(ceil(this.length/n)).fill().map((_,i) => this.slice(i*n,i*n+n));
+    }});
+
+    $("#company-list")
+        .append(companyHandlebar( {list: company.chunk(4)}));
+
 });
