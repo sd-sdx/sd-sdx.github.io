3680610fc6389a117d9d669a0cbc48d1285ca31e
Scott Tran <scott.tran@gmail.com>
2017-07-05 08:10:23 -0700

    publish schedule

--- js/sdx.js
+++ js/sdx.js
@@ -1,32 +1,39 @@
 require([
     'js/jquery.js',
     'js/handlebars.js',
-    'text!session.handlebars',
-    'text!sessions.json',
+    'text!schedule.handlebars',
+    'text!schedule.json',
     'text!companies.handlebars',
     'text!companies.json'
-], function (_, handlebars,  sessionTemplate, sessionData, companyTemplate, companyData) {
-    var template = handlebars.compile(sessionTemplate);
+], function (_, handlebars,  scheduleTemplate, scheduleData, companyTemplate, companyData) {
+    var template = handlebars.compile(scheduleTemplate);
     var companyHandlebar= handlebars.compile(companyTemplate);
 
-    var content = JSON.parse(sessionData);
+    var content = JSON.parse(scheduleData);
     var company = JSON.parse(companyData);
 
-    $("#customer-support-skills")
-        .append(template( {session: content["Customers Support Skills"]}));
-    $("#manager-skills")
-        .append(template( {session: content["Manager Skills"]}));
-    $("#technical-skills")
-        .append(template( {session: content["Technical Skills"]}));
-    $("#self-skills")
-        .append(template( {session: content["Self Skills"]}));
-    var ceil = Math.ceil;
+    $("#9-10").append(template( {schedule: content["9-10"]}));
+    $("#10-11").append(template( {schedule: content["10-11"]}));
+    $("#11-12").append(template( {schedule: content["11-12"]}));
+    $("#2-3").append(template( {schedule: content["2-3"]}));
+    $("#3-4").append(template( {schedule: content["3-4"]}));
+    $("#4-5").append(template( {schedule: content["4-5"]}));
 
+    var ceil = Math.ceil;
     Object.defineProperty(Array.prototype, 'chunk', {value: function(n) {
         return Array(ceil(this.length/n)).fill().map((_,i) => this.slice(i*n,i*n+n));
     }});
 
     $("#company-list")
         .append(companyHandlebar( {list: company.chunk(4)}));
+
+    $("div.tab-menu>div.list-group>a").click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.tab>div.tab-content").removeClass("active");
+        $("div.tab>div.tab-content").eq(index).addClass("active");
+    });
 
 });
